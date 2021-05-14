487fa34f21356ead83daf148ef23fb8e21395f33
olegskrivickis <78209436+olegskrivickis@users.noreply.github.com>
2021-05-14 14:52:01 +0300

    Add files via upload

---
+++ 4uzdevums.py
@@ -0,0 +1,11 @@
+ """
+Uzrakstiet programmu Python, lai dotajā sarakstā saskaitītu 
+cik skaitļi 2 ir  dotajā sarakstā.
+
+Testēšanas dati:
+saraksts - [2,6,2,32,2,17]
+Atgriežamā vērtība - Sarakstā ir 3 prasītie skaitļi
+"""
+saraksts = [2,6,2,32,2,17]
+sk1 = saraksts.count('2')
+print(saraksts,)
